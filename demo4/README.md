500d7a5f1e48f742045e849c2d3e5dc89c21e3e0
z15746 <1801001874@qq.com>
2018-08-23 22:20:21 +0800

    ADD：Spring Boot中使用Spring Security进行安全控制

---
+++ demo4/README.md
@@ -0,0 +1,16 @@
+# Learn-Spring-Boot
+
+## 安全管理
+Spring Boot中使用Spring Security进行安全控制
+
+**注意：** 
+```
+<parent>
+		<groupId>org.springframework.boot</groupId>
+		<artifactId>spring-boot-starter-parent</artifactId>
+		<version>1.3.2.RELEASE</version>
+		<relativePath/> <!-- lookup parent from repository -->
+</parent>
+```
+
+中Spring-Boot版本为1.3.2的时候登陆成功才会有跳转页，否则别的版本不会出现跳转页，具体原因待考究。
---
+++ demo4/src/main/java/com/example/demo4/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.demo4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/home")
+    @ResponseBody
+    public String home() {
+        return "Welcome to home";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+}
